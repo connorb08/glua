48b2e9840081f63c1e35a0989048de5f01daa71e
Connor Bray <connorb108@gmail.com>
2024-05-29 17:56:41 -0400

    net message

--- lua/autorun/shared.lua
+++ lua/autorun/shared.lua
@@ -1,5 +1,6 @@
 SPEED_BOOST_MULTIPLIER = 10
 HEALTH_BOOST_MULTIPLIER = 10
+TOKEN_COST = 1000
 
 player_credits = player_credits or {}
 player_health_boost = player_health_boost or {}
@@ -70,63 +71,89 @@ hook.Add( "PlayerButtonDown", "OpenMenu", function( ply, button )
 		return
 	end
 
+    if SERVER then
+        return
+    end
+
     local frame = vgui.Create("DFrame") -- The name of the panel we don't have to parent it.
     frame:SetPos(100, 100) -- Set the position to 100x by 100y. 
     frame:SetSize(800, 500) -- Set the size to 300x by 200y.
     frame:SetTitle("Derma Frame") -- Set the title in the top left to "Derma Frame".
     frame:MakePopup() -- Makes your mouse be able to move around.
 
-    local credits = vgui.Create( "DLabel", frame ) // Create the button and parent it to the frame
-    credits:SetText( string.format("Credits: %d", 5) )					// Set the text on the button
+    local credits = vgui.Create( "DLabel", frame )
+    credits:SetText( string.format("Credits: %d", 5) )
     credits:SetPos( 25, 50 )
     credits:SetSize( 250, 30 )
 
-    local health = vgui.Create( "DLabel", frame ) // Create the button and parent it to the frame
-    health:SetText( string.format("health: %d", player_health_boost.GetHealthBoost(ply)) )					// Set the text on the button
+    local health = vgui.Create( "DLabel", frame )
+    health:SetText( string.format("health: %d", player_health_boost.GetHealthBoost(ply)) )
     health:SetPos( 25, 75 )
     health:SetSize( 250, 30 )
 
-    local buy_credits = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-    buy_credits:SetText( "Buy Credits" )					// Set the text on the button
-    buy_credits:SetPos( 25, 100 )					// Set the position on the frame
-    buy_credits:SetSize( 250, 30 )					// Set the size
-    buy_credits.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
-        RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
+    local buy_credits = vgui.Create( "DButton", frame )
+    buy_credits:SetText( "Buy Credits" )
+    buy_credits:SetPos( 25, 100 )
+    buy_credits:SetSize( 250, 30 )
+    buy_credits.DoClick = function()
+        RunConsoleCommand( "say", "Purchase tokens" )
+        net.Start( "purchase_token" )
+		net.SendToServer()
     end
 
-    local increase_health = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-    increase_health:SetText( "Increase Health" )					// Set the text on the button
-    increase_health:SetPos( 25, 150 )					// Set the position on the frame
-    increase_health:SetSize( 250, 30 )					// Set the size
-    increase_health.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
+    local increase_health = vgui.Create( "DButton", frame )
+    increase_health:SetText( "Increase Health" )
+    increase_health:SetPos( 25, 150 )
+    increase_health:SetSize( 250, 30 )
+    increase_health.DoClick = function()
         -- RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
         player_health_boost.AddHealthBoost(ply, 1)
         print(player_health_boost)
     end
 
-    local decrease_health = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-    decrease_health:SetText( "Decrease Health" )					// Set the text on the button
-    decrease_health:SetPos( 25, 200 )					// Set the position on the frame
-    decrease_health:SetSize( 250, 30 )					// Set the size
-    decrease_health.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
-        RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
+    local decrease_health = vgui.Create( "DButton", frame )
+    decrease_health:SetText( "Decrease Health" )
+    decrease_health:SetPos( 25, 200 )
+    decrease_health:SetSize( 250, 30 )
+    decrease_health.DoClick = function()
+        RunConsoleCommand( "say", "Hi" )
     end
 
-    local increase_speed = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-    increase_speed:SetText( "Increase Speed" )					// Set the text on the button
-    increase_speed:SetPos( 25, 250 )					// Set the position on the frame
-    increase_speed:SetSize( 250, 30 )					// Set the size
-    increase_speed.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
-        RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
+    local increase_speed = vgui.Create( "DButton", frame ) 
+    increase_speed:SetText( "Increase Speed" )				
+    increase_speed:SetPos( 25, 250 )					
+    increase_speed:SetSize( 250, 30 )					
+    increase_speed.DoClick = function()				
+        RunConsoleCommand( "say", "Hi" )			
     end
 
-    local decrease_speed = vgui.Create( "DButton", frame ) // Create the button and parent it to the frame
-    decrease_speed:SetText( "Decrease Speed" )					// Set the text on the button
-    decrease_speed:SetPos( 25, 300 )					// Set the position on the frame
-    decrease_speed:SetSize( 250, 30 )					// Set the size
-    decrease_speed.DoClick = function()				// A custom function run when clicked ( note the . instead of : )
-        RunConsoleCommand( "say", "Hi" )			// Run the console command "say hi" when you click it ( command, args )
+    local decrease_speed = vgui.Create( "DButton", frame )
+    decrease_speed:SetText( "Decrease Speed" )			
+    decrease_speed:SetPos( 25, 300 )					
+    decrease_speed:SetSize( 250, 30 )					
+    decrease_speed.DoClick = function()				
+        RunConsoleCommand( "say", "Hi" )			
     end
 
+    local printCredits = vgui.Create( "DButton", frame )
+    printCredits:SetText( "Print" )					
+    printCredits:SetPos( 25, 350 )					
+    printCredits:SetSize( 250, 30 )					
+    printCredits.DoClick = function()				
+        for k,v in pairs(player_credits) do
+            print(k, v)
+        end
+    end
 
 end)
+
+if ( SERVER ) then
+	util.AddNetworkString( "purchase_token" )
+	net.Receive( "purchase_token", function( len, ply )
+        local money = ply:getDarkRPVar("money");
+        if (money >= TOKEN_COST) then
+            player_credits.AddCredits(ply, 1)
+            ply:addMoney(-TOKEN_COST)
+        end
+	end )
+end
